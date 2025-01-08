b5ee4f22cedabcb182e9b2a6ac4f70e66a636e7e
zulianc <julien.chataigner@etu.unic-lyon1.fr>
2025-01-08 10:49:05 +0100

    implémenté visitWhile

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -344,16 +344,39 @@ public class CodeGenerator  extends AbstractParseTreeVisitor<Program> implements
 
     @Override
     public Program visitWhile(grammarTCLParser.WhileContext ctx) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visitWhile'");
+        // WHILE '(' expr ')' instr
+        /* pseudo-assembler while(i<10) instr
+        loop: visit(cond)
+            XOR R1 R1 R1
+            JEQU nextRegister-2 nextRegister-1 end_loop //-2 = cond return, -1 = XOR'd register
+            instr
+            JMP loop
+        end_loop: suite...
+        */
+        Program program = new Program();
+        String labelStartLoop = this.getLabel();
+        String labelEndLoop = this.getLabel();
+        Program condLoopProgram = visit(ctx.getChild(2)); // loop condition
+        condLoopProgram.getInstructions().getFirst().setLabel(labelStartLoop); // set looping label
+        program.addInstructions(condLoopProgram);
+        program.addInstruction(new UAL(UAL.Op.XOR, nextRegister, nextRegister, nextRegister)); // set new register to 0 for later test
+        nextRegister++;
+        program.addInstruction(new CondJump(CondJump.Op.JEQU, nextRegister-2, nextRegister-1, labelEndLoop)); // test if condition is false => stop looping
+        program.addInstructions(visit(ctx.getChild(4))); // instructions inside the loop
+        program.addInstruction(new JumpCall(JumpCall.Op.JMP, labelStartLoop)); // go back to the start of the loop
+        Program endLoopProgram = new Program();
+        endLoopProgram.addInstruction(new UALi(UALi.Op.ADD, nextRegister, nextRegister, 0)); // dummy instruction to set end loop label
+        endLoopProgram.getInstructions().getFirst().setLabel(labelEndLoop);
+        program.addInstructions(endLoopProgram);
+        return program;
     }
 
     @Override
     public Program visitFor(grammarTCLParser.ForContext ctx) {
-        //FOR '(' instr ',' expr ',' instr ')' instr
-        /* pseudo-assembler for(int i = 0; i < 10; i++)
+        // FOR '(' instr ',' expr ',' instr ')' instr
+        /* pseudo-assembler for(int i = 0; i < 10; i++) instr
             ST R0 1
-        loop: visit(expr)
+        loop: visit(cond)
             XOR R1 R1 R1
             JEQU nextRegister-2 nextRegister-1 end_loop //-2 = cond return, -1 = XOR'd register
             instr
@@ -364,18 +387,18 @@ public class CodeGenerator  extends AbstractParseTreeVisitor<Program> implements
         Program program = new Program();
         String labelStartLoop = this.getLabel();
         String labelEndLoop = this.getLabel();
-        program.addInstructions(visit(ctx.getChild(2))); //initialization
-        Program condLoopProgram = visit(ctx.getChild(4)); //loop condition
-        condLoopProgram.getInstructions().getFirst().setLabel(labelStartLoop); //set looping label
+        program.addInstructions(visit(ctx.getChild(2))); // initialization
+        Program condLoopProgram = visit(ctx.getChild(4)); // loop condition
+        condLoopProgram.getInstructions().getFirst().setLabel(labelStartLoop); // set looping label
         program.addInstructions(condLoopProgram);
-        program.addInstruction(new UAL(UAL.Op.XOR, nextRegister, nextRegister, nextRegister)); //set new register to 0 for later test
+        program.addInstruction(new UAL(UAL.Op.XOR, nextRegister, nextRegister, nextRegister)); // set new register to 0 for later test
         nextRegister++;
-        program.addInstruction(new CondJump(CondJump.Op.JEQU, nextRegister-2, nextRegister-1, labelEndLoop)); //test if condition is false => stop looping
-        program.addInstructions(visit(ctx.getChild(8))); //instructions inside the loop
-        program.addInstructions(visit(ctx.getChild(6))); //iteration
-        program.addInstruction(new JumpCall(JumpCall.Op.JMP, labelStartLoop)); //go back to the start of the loop
+        program.addInstruction(new CondJump(CondJump.Op.JEQU, nextRegister-2, nextRegister-1, labelEndLoop)); // test if condition is false => stop looping
+        program.addInstructions(visit(ctx.getChild(8))); // instructions inside the loop
+        program.addInstructions(visit(ctx.getChild(6))); // iteration
+        program.addInstruction(new JumpCall(JumpCall.Op.JMP, labelStartLoop)); // go back to the start of the loop
         Program endLoopProgram = new Program();
-        endLoopProgram.addInstruction(new UALi(UALi.Op.ADD, nextRegister, nextRegister, 0)); //dummy instruction to set end loop label
+        endLoopProgram.addInstruction(new UALi(UALi.Op.ADD, nextRegister, nextRegister, 0)); // dummy instruction to set end loop label
         endLoopProgram.getInstructions().getFirst().setLabel(labelEndLoop);
         program.addInstructions(endLoopProgram);
         return program;
@@ -383,54 +406,54 @@ public class CodeGenerator  extends AbstractParseTreeVisitor<Program> implements
 
     @Override
     public Program visitReturn(grammarTCLParser.ReturnContext ctx) {
-        //RETURN expr SEMICOL
+        // RETURN expr SEMICOL
         Program program = new Program();
-        program.addInstructions(visit(ctx.getChild(1))); //expr, return value will be in R(nextRegister-1)
+        program.addInstructions(visit(ctx.getChild(1))); // expr, return value will be in R(nextRegister-1)
         return program;
     }
 
     @Override
     public Program visitCore_fct(grammarTCLParser.Core_fctContext ctx) {
-        //core_fct: '{' instr* RETURN expr SEMICOL '}';
+        // core_fct: '{' instr* RETURN expr SEMICOL '}';
         Program program = new Program();
         int nbInstructions = ctx.getChildCount() - 5;
-        for (int i = 0; i < nbInstructions; i++) { //instr*
+        for (int i = 0; i < nbInstructions; i++) { // instr*
             program.addInstructions(visit(ctx.getChild(i + 1)));
         }
-        program.addInstructions(visit(ctx.getChild(ctx.getChildCount() - 3))); //expr
+        program.addInstructions(visit(ctx.getChild(ctx.getChildCount() - 3))); // expr
         return program;
     }
 
     @Override
     public Program visitDecl_fct(grammarTCLParser.Decl_fctContext ctx) {
-        //decl_fct: type VAR '(' (type VAR (',' type VAR)*)? ')' core_fct;
+        // decl_fct: type VAR '(' (type VAR (',' type VAR)*)? ')' core_fct;
         Program program = new Program();
         int nbChilds = ctx.getChildCount();
         int nbArguments = ((nbChilds - 5) == 0 ? 0 : (nbChilds - 4)/2);
-        for(int i = 0; i < nbArguments; i++) { //the arguments are stacked before the call so we unstack them
+        for(int i = 0; i < nbArguments; i++) { // the arguments are stacked before the call so we unstack them
             program.addInstruction(new Mem(Mem.Op.LD, nextRegister, SP));
             nextRegister++;
             program.addInstruction(new UALi(UALi.Op.SUB, SP, SP, 1));
         }
-        program.getInstructions().getFirst().setLabel(ctx.getChild(2).toString()); //function label
-        program.addInstructions(visit(ctx.getChild(nbChilds - 1))); //core_fct
+        program.getInstructions().getFirst().setLabel(ctx.getChild(2).toString()); // function label
+        program.addInstructions(visit(ctx.getChild(nbChilds - 1))); // core_fct
         return program;
     }
 
     @Override
     public Program visitMain(grammarTCLParser.MainContext ctx) {
-        //main: decl_fct* 'int main()' core_fct EOF
+        // main: decl_fct* 'int main()' core_fct EOF
         Program program = new Program();
-        program.addInstruction(new UAL(UAL.Op.XOR, 0, 0, 0)); //initialize SP
-        program.addInstruction(new JumpCall(JumpCall.Op.JMP, "main")); //call main
+        program.addInstruction(new UAL(UAL.Op.XOR, 0, 0, 0)); // initialize SP
+        program.addInstruction(new JumpCall(JumpCall.Op.JMP, "main")); // call main
         int nbChilds = ctx.getChildCount();
-        for(int i = 0; i < nbChilds - 3; i++) { //decl_fct*
+        for(int i = 0; i < nbChilds - 3; i++) { // decl_fct*
             program.addInstructions(visit(ctx.getChild(i)));
         }
-        Program mainCoreProgram = visit(ctx.getChild(nbChilds - 2)); //core_fct
-        mainCoreProgram.getInstructions().getFirst().setLabel("main"); //main label
+        Program mainCoreProgram = visit(ctx.getChild(nbChilds - 2)); // core_fct
+        mainCoreProgram.getInstructions().getFirst().setLabel("main"); // main label
         program.addInstructions(mainCoreProgram);
-        program.addInstruction(new Stop()); //STOP
+        program.addInstruction(new Stop()); // STOP
         return program;
     }
 }
