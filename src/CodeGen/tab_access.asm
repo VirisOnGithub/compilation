e2d2bde6b30a6e3a4abd3e2d7e0fce31edf2246b
Persson-dev <sim16.prib@gmail.com>
2025-01-14 22:08:01 +0100

    tab_access assembly

---
+++ src/CodeGen/tab_access.asm
@@ -0,0 +1,35 @@
+tab_access:
+	XOR R6 R6 R6	# constante à 0
+	SUBi SP SP 1
+	LD R0 SP		# index
+	SUBi SP SP 1
+	LD R1 SP		# pointeur du tableau
+	LD R2 R1		# longueur du tableau
+	JIEQ R0 R2 skip_resize
+	ADDi R2 R0 1	# longueur = index + 1
+	ST R1 R2		# on met à jour la longueur
+skip_resize:
+	ADDi R1 R1 1	# pointeur++
+	XOR R3 R3 R3	# i = 0
+begin_loop:
+	XOR R4 R4 R4
+	MODi R5 R3 10
+	JNEQ R5 R4 skip_tab_end	# i % 10 != 0
+	JEQU R3 R4 skip_tab_end	# i == 0
+	JIEQ R3 R2 skip_alloc
+	ST TP R1			# *pointeur = TP
+	ADDi TP TP 11		# TP += 11
+skip_alloc:
+	LD R1 R1			# pointeur = *pointeur
+skip_tab_end:
+	JIEQ R3 R2 skip_fill
+	ST R6 R1
+skip_fill:
+	JNEQ R3 R0 skip_return
+	LD R1 R1 			# pointeur = *pointeur
+	ST R1 SP			# on empile la valeur dans le tableau
+	ADDi SP SP 1
+	RET					# et on retourne
+skip_return:
+	ADDi R3 R3 R3		# i++
+	JMP begin_loop
