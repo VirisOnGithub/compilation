c6867387d6300b3f6fe33be9d4f1c43ca305d56c
Persson-dev <sim16.prib@gmail.com>
2025-01-15 12:15:02 +0100

    t'as rien vu

--- src/VarStack.java
+++ src/VarStack.java
@@ -23,32 +23,35 @@ public class VarStack<T> {
 	}
 
 	public void enterFunction() {
-        lastAccessibleDepth.add(stack.size());
-        this.enterBlock();
-    }
+		lastAccessibleDepth.add(stack.size());
+		this.enterBlock();
+	}
 
 	public void leaveFunction() {
-        this.leaveBlock();
-        lastAccessibleDepth.pop();
-    }
+		this.leaveBlock();
+		lastAccessibleDepth.pop();
+	}
 
 	public T getVarRegister(T varName) {
-        for (int depth = stack.size() - 1; depth >= lastAccessibleDepth.getLast(); depth--) { // we unstack all the accessible maps in varRegisters
-            var varMap = stack.get(depth);
-            if (varMap.containsKey(varName)) { // we stop at the first corresponding variable name
-                return varMap.get(varName);
-            }
-        }
-        throw new RuntimeException("The variable " + varName + " has not been assigned"); // if none was found
-    }
+		for (int depth = stack.size() - 1; depth >= lastAccessibleDepth.getLast(); depth--) { // we unstack all the
+																								// accessible maps in
+																								// varRegisters
+			var varMap = stack.get(depth);
+			if (varMap.containsKey(varName)) { // we stop at the first corresponding variable name
+				return varMap.get(varName);
+			}
+		}
+		throw new RuntimeException("The variable " + varName + " has not been assigned"); // if none was found
+	}
 
 	/**
-     * Set the register number of a given variable
-     * @param varName the name of the variable
-     * @param value the value we want to map the variable to
-     */
-    public void assignVar(String varName, T value) {
-        this.stack.getLast().put(varName, value);
-    }
+	 * Set the register number of a given variable
+	 * 
+	 * @param varName the name of the variable
+	 * @param value   the value we want to map the variable to
+	 */
+	public void assignVar(String varName, T value) {
+		this.stack.getLast().put(varName, value);
+	}
 
 }
